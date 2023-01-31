481c684178bc523e219916212992e192255ed618
Erik Trejo Lujano <85711028+eTrejoLujano@users.noreply.github.com>
2023-01-31 15:19:55 -0800

    LearnColors

---
+++ LearningSwift/ColorsBootcamp.swift
@@ -0,0 +1,29 @@
+//
+//  ColorsBootcamp.swift
+//  LearningSwift
+//
+//  Created by ErikT on 1/31/23.
+//
+
+import SwiftUI
+
+struct ColorsBootcamp: View {
+    var body: some View {
+        RoundedRectangle(cornerRadius: 25.0)
+            .fill(
+//                Color.primary
+//                Color(#colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1))
+//                Color(UIColor.secondarySystemBackground)
+                Color("CustomColor")
+            )
+            .frame(width: 300, height: 200)
+//            .shadow(radius:10)
+            .shadow(color: Color("CustomColor").opacity(0.3), radius: 10, x: -20, y: -20)
+    }
+}
+
+struct ColorsBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        ColorsBootcamp()
+    }
+}
